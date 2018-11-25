14fad759469d72019bace46580e78b571a131991
MagDry <magdalena.drymajlo@gmail.com>
2018-11-25 22:49:51 +0100

    list icon

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    function buttonClick() {
-        console.log('Button clicked!');
-    };
+    // function buttonClick() {
+    //     console.log('Button clicked!');
+    // };
 
-    document.getElementById('add').addEventListener('click', buttonClick); 
+    // document.getElementById('add').addEventListener('click', buttonClick); 
+
+    //User click on the add button
+    //If there is any text inside the item field, add that text to the todo list
+
+    document.getElementById('add').addEventListener('click', function() {
+        let value = document.getElementById('item').value;
+        // console.log(value);
+
+        if (value) {
+            // console.log('There was a value, heh');
+        }
+    }); 
 
 });
